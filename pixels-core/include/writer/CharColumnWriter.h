39ec93b3bca8ee2004dfd80470ce4020a34348f6
Poppy-lu <3314560760@qq.com>
2025-01-24 14:36:04 +0800

    second

--- pixels-core/include/writer/CharColumnWriter.h
+++ pixels-core/include/writer/CharColumnWriter.h
@@ -25,4 +25,28 @@
 #ifndef DUCKDB_CHARCOLUMNWRITER_H
 #define DUCKDB_CHARCOLUMNWRITER_H
 
+#include "writer/ColumnWriter.h"
+#include "type/TypeDescription.h"
+#include "option/PixelsWriterOption.h"
+#include "vector/ColumnVector.h"
+
+class CharColumnWriter : public ColumnWriter {
+public:
+    CharColumnWriter(std::shared_ptr<TypeDescription> type, std::shared_ptr<PixelsWriterOption> writerOption);
+
+    int write(std::shared_ptr<ColumnVector> vector, int length) override;
+    void close() override;
+    void newPixel() override;
+    pixels::proto::ColumnEncoding getColumnChunkEncoding() const override;
+
+private:
+    void writeCurPartChar(std::shared_ptr<ColumnVector> columnVector, char* values, int curPartLength, int curPartOffset);
+    bool decideNullsPadding(std::shared_ptr<PixelsWriterOption> writerOption);
+
+    bool runlengthEncoding; // 是否使用运行长度编码（RLE）
+    std::unique_ptr<RunLenIntEncoder> encoder; // 运行长度编码器
+    std::vector<char> curPixelVector; // 当前像素的值向量
+};
+
 #endif // DUCKDB_CHARCOLUMNWRITER_H
+
--- pixels-core/lib/writer/CharColumnWriter.cpp
+++ pixels-core/lib/writer/CharColumnWriter.cpp
@@ -17,3 +17,117 @@
  * License along with Pixels.  If not, see
  * <https://www.gnu.org/licenses/>.
  */
+#include "writer/CharColumnWriter.h"
+#include "utils/BitUtils.h"
+#include "encoding/EncodingUtils.h"
+#include "io/ByteBuffer.h"
+#include <iostream>
+
+// 构造函数：初始化 CharColumnWriter
+CharColumnWriter::CharColumnWriter(std::shared_ptr<TypeDescription> type, std::shared_ptr<PixelsWriterOption> writerOption)
+    : ColumnWriter(type, writerOption), curPixelVector(pixelStride) {
+    // 根据编码级别决定是否使用运行长度编码
+    runlengthEncoding = writerOption->getEncodingLevel().ge(EncodingLevel::Level::EL2);
+    if (runlengthEncoding) {
+        encoder = std::make_unique<RunLenIntEncoder>();
+    }
+}
+
+// 写入数据：将 ColumnVector 中的字符数据写入当前像素
+int CharColumnWriter::write(std::shared_ptr<ColumnVector> vector, int size) {
+    std::cout << "In CharColumnWriter" << std::endl;
+    auto columnVector = std::static_pointer_cast<CharColumnVector>(vector);
+    if (!columnVector) {
+        throw std::invalid_argument("Invalid vector type");
+    }
+
+    char* values = columnVector->charVector; // 获取字符数据
+
+    int curPartLength;         // 当前像素分区的长度
+    int curPartOffset = 0;     // 当前像素分区的起始偏移量
+    int nextPartLength = size; // 下一个像素分区的长度
+
+    // 分区逻辑：将数据划分为当前像素和下一个像素
+    while ((curPixelIsNullIndex + nextPartLength) >= pixelStride) {
+        curPartLength = pixelStride - curPixelIsNullIndex;
+        writeCurPartChar(columnVector, values, curPartLength, curPartOffset);
+        newPixel();
+        curPartOffset += curPartLength;
+        nextPartLength = size - curPartOffset;
+    }
+
+    curPartLength = nextPartLength;
+    writeCurPartChar(columnVector, values, curPartLength, curPartOffset);
+
+    return outputStream->getWritePos();
+}
+
+void CharColumnWriter::close() {
+    if (runlengthEncoding && encoder) {
+        encoder->clear(); // 清理编码器资源
+    }
+    ColumnWriter::close();
+}
+
+// 写入当前分区字符数据
+void CharColumnWriter::writeCurPartChar(std::shared_ptr<ColumnVector> columnVector, char* values, int curPartLength, int curPartOffset) {
+    for (int i = 0; i < curPartLength; i++) {
+        curPixelEleIndex++;
+        if (columnVector->isNull[i + curPartOffset]) {
+            hasNull = true;
+            if (nullsPadding) {
+                // 对空值进行填充处理
+                curPixelVector[curPixelVectorIndex++] = '\0';
+            }
+        } else {
+            curPixelVector[curPixelVectorIndex++] = values[i + curPartOffset];
+        }
+    }
+    // 将空值标记复制到当前像素的空值数组中
+    std::copy(columnVector->isNull + curPartOffset, columnVector->isNull + curPartOffset + curPartLength, isNull.begin() + curPixelIsNullIndex);
+    curPixelIsNullIndex += curPartLength;
+}
+
+// 决定是否填充空值
+bool CharColumnWriter::decideNullsPadding(std::shared_ptr<PixelsWriterOption> writerOption) {
+    if (writerOption->getEncodingLevel().ge(EncodingLevel::Level::EL2)) {
+        return false; // 如果编码级别高于 EL2，则不填充空值
+    }
+    return writerOption->isNullsPadding();
+}
+
+// 创建新像素：将当前像素数据写入输出流
+void CharColumnWriter::newPixel() {
+    if (runlengthEncoding) {
+        std::vector<byte> buffer(curPixelVectorIndex * sizeof(char));
+        int resLen;
+        encoder->encode(curPixelVector.data(), buffer.data(), curPixelVectorIndex, resLen);
+        outputStream->putBytes(buffer.data(), resLen);
+    } else {
+        std::shared_ptr<ByteBuffer> curVecPartitionBuffer = std::make_shared<ByteBuffer>(curPixelVectorIndex * sizeof(char));
+        EncodingUtils encodingUtils;
+        if (byteOrder == ByteOrder::PIXELS_LITTLE_ENDIAN) {
+            for (int i = 0; i < curPixelVectorIndex; i++) {
+                encodingUtils.writeCharLE(curVecPartitionBuffer, curPixelVector[i]);
+            }
+        } else {
+            for (int i = 0; i < curPixelVectorIndex; i++) {
+                encodingUtils.writeCharBE(curVecPartitionBuffer, curPixelVector[i]);
+            }
+        }
+        outputStream->putBytes(curVecPartitionBuffer->getPointer(), curVecPartitionBuffer->getWritePos());
+    }
+
+    ColumnWriter::newPixel();
+}
+
+// 获取列块编码方式
+pixels::proto::ColumnEncoding CharColumnWriter::getColumnChunkEncoding() const {
+    pixels::proto::ColumnEncoding columnEncoding;
+    if (runlengthEncoding) {
+        columnEncoding.set_kind(pixels::proto::ColumnEncoding::Kind::ColumnEncoding_Kind_RUNLENGTH);
+    } else {
+        columnEncoding.set_kind(pixels::proto::ColumnEncoding::Kind::ColumnEncoding_Kind_NONE);
+    }
+    return columnEncoding;
+}
